82c931b34151968b70755522a24d5dfe19efb429
sulakshanadeore <deoresulakshana@gmail.com>
2025-10-23 19:31:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using GenericsLibraryDemo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // List<Employee> emps = new List<Employee> 
+        // {
+        // new Employee{Empid=10,EmpName="Jill",Deptno=10 },
+        // new Employee{ Empid=11,EmpName="Jim",Deptno=20},
+        // new Employee{ Empid=12,EmpName="Jack",Deptno=30}
+
+        // };
+
+
+        //List<Employee> emplist = new List<Employee>(); 
+
+        // //create employee object
+        // Employee employee = new Employee();
+        // employee.Empid = 1;
+        // employee.EmpName = "Raj";
+        // employee.Deptno = 10;
+
+        // //add employee object to list
+        // emplist.Add(employee);
+
+
+        // //Collection Initializer
+        // emplist.Add(new Employee { Empid=2,EmpName="Ratan",Deptno=10});
+
+        // //Object Initialiser-- create employee object
+        // Employee e2 = new Employee {Empid=3,EmpName="Jack",Deptno=20 };
+        // //add object to list
+        // emplist.Add(e2);
+
+
+        char ans = 'Y';
+        EmployeeService employeeService = new EmployeeService();
+
+        Console.WriteLine("1.Add \n2.Remove \n3.Update \n4.Find \n5.ShowAll \n6.Exit");
+        
+        while (ans=='Y')
+        {
+            Console.WriteLine("Enter your choice");
+            int choice=Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Employee emp=new Employee();
+                    Console.WriteLine("Enter EmployeeID");
+                    emp.Empid = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Name");
+                    emp.EmpName = Console.ReadLine().Trim();
+                    Console.WriteLine("Enter Deptno");
+                    emp.Deptno=Convert.ToInt32(Console.ReadLine());
+                    employeeService.NewEmployee(emp);
+                    break;
+                case 2: break;
+                case 3:
+                    Console.WriteLine("Enter EmployeeID");
+                    int empid = Convert.ToInt32(Console.ReadLine());
+
+                    Employee emptoUpdate=new Employee();
+                    Console.WriteLine("Enter Name");
+                    emptoUpdate.EmpName = Console.ReadLine().Trim();
+                    Console.WriteLine("Enter Deptno");
+                    emptoUpdate.Deptno = Convert.ToInt32(Console.ReadLine());
+
+                    employeeService.UpdateEmployeeData(empid, emptoUpdate); 
+                    break;
+                case 4: break;
+                case 5:
+                    List<Employee> allemps=employeeService.GetAllEmpList();
+                    foreach (var item in allemps)
+                    {
+                        Console.WriteLine($"{item.Empid}|{item.EmpName}|{item.Deptno}");
+
+                    }
+                    break;
+                case 6: break;
+
+            }
+            Console.WriteLine("Do u want to continue");
+            ans=Convert.ToChar(Console.ReadLine()); 
+
+        }
+
+
+           
+    }
+}
